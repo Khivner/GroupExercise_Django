8591453b9d395814857ef1b313dff5773951fb9d
Khivner <khivner@gmail.com>
2017-06-04 16:04:14 -0400

    added static root variable to settings

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
-git clone https://github.com/Khivner/GroupExercise_Django ~/staging.groupexercise.khivner.net/htdocs
-virtualenv ~/staging.groupexercise.khivner.net/htdocs
-source ~/staging.groupexercise.khivner.net/htdocs/bin/activate
-pip3 install django 
-pip3 install mysqlclient
-pip3 install gunicorn
 
-ln -s ~/staging.groupexercise.khivner.net/shared/SECRETKEY.py ~/staging.groupexercise.khivner.net/htdocs/mysite
-~/staging.groupexercise.khivner.net/mysite/manage.py migrate
+PROJECT_FOLDER=/home/khivner/staging.groupexercise.khivner.net/htdocs
+SHARED_FOLDER=/home/khivner/staging.groupexercise.khivner.net/shared
+GIT_URL=https://github.com/Khivner/GroupExercise_Django
+CURRENT_BRANCH=master
 
+# Clone if not exists
+if [ ! -d "$PROJECT_FOLDER" ] ; then
+	git clone "$GIT_URL" "PROJECT_FOLDER"
+fi
 
+# Update project to current branch
+cd $PROJECT_FOLDER && git fetch --all 
+cd $PROJECT_FOLDER && git checkout --force "origin/$CURRENT_BRANCH"
+
+# symlink all relevant items
+ln -sf $SHARED_FOLDER/SECRETKEY.py $PROJECT_FOLDER/mysite
+ln -sf $SHARED_FOLDER/bin $PROJECT_FOLDER
+ln -sf $SHARED_FOLDER/tmp $PROJECT_FOLDER
+
+# install necessary packages
+
+$PROJECT_FOLDER/bin/pip3 install django 
+$PROJECT_FOLDER/bin/pip3 install mysqlclient
+$PROJECT_FOLDER/bin/pip3 install gunicorn
+
+# Run Migrations
+$PROJECT_FOLDER/bin/python3 $PROJECT_FOLDER/manage.py migrate
+
+sudo /sbin/reload khivner.groupexercise.staging
+
+sudo /sbin/reload khivner.groupexercise.staging
